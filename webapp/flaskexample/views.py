22adee661bed9ad8c0793c9afc85bdbbac126f51
Anastasia <anikouli@gmail.com>
2020-06-14 18:10:57 -0400

    Update web app and Lyrics_generator.ipynb

--- webapp/flaskexample/views.py
+++ webapp/flaskexample/views.py
@@ -1,67 +1,50 @@
 from flask import render_template
 from flask import request
 from flaskexample import app
-from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
-import pandas as pd
-import psycopg2
+#from sqlalchemy import create_engine
+#from sqlalchemy_utils import database_exists, create_database
+#import pandas as pd
+#import psycopg2
+import gpt_2_simple as gpt2
 
 # Python code to connect to Postgres
 # You may need to modify this based on your OS, 
 # as detailed in the postgres dev setup materials.
-user = 'stasianik' #add your Postgres username here      
-host = 'localhost'
-dbname = 'birth_db'
-db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
-con = None
-con = psycopg2.connect(database = dbname, user = user)
+#user = 'stasianik' #add your Postgres username here      
+#host = 'localhost'
+#dbname = 'birth_db'
+#db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
+#con = None
+#con = psycopg2.connect(database = dbname, user = user)
+
+# Load in model checkpoint and start a tensorflow session
+#sess = gpt2.start_tf_sess()
+#gpt2.load_gpt2(sess, run_name='run1')
 
 @app.route('/')
 @app.route('/index')
 def index():
    return render_template("index.html",
-      title = 'Home', user = { 'nickname': 'Miguel' },
+      title = 'Home'
       )
-
-@app.route('/db')
-def birth_page():
-   sql_query = """                                                                       
-               SELECT * FROM birth_data_table WHERE delivery_method='Cesarean';          
-               """
-   query_results = pd.read_sql_query(sql_query,con)
-   births = ""
-   for i in range(0,10):
-       births += query_results.iloc[i]['birth_month']
-       births += "<br>"
-   return births 
-
-@app.route('/db_fancy')
-def cesareans_page_fancy():
-   sql_query = """
-              SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean';
-               """
-   query_results=pd.read_sql_query(sql_query,con)
-   births = []
-   for i in range(0,query_results.shape[0]):
-       births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-   return render_template('cesareans.html',births=births)
            
 @app.route('/input')
-def cesareans_input():
+def lyrics_input():
    return render_template("input.html")
 
 @app.route('/output')
-def cesareans_output():
-   #pull 'birth_month' from input field and store it
-   patient = request.args.get('birth_month')
-   #just select the Cesareans  from the birth dtabase for the month that the user inputs
-   query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
-   print(query)
-   query_results=pd.read_sql_query(query,con)
-   print(query_results)
-   births = []
-   for i in range(0,query_results.shape[0]):
-      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-   the_result = ''
-   return render_template("output.html", births = births, the_result = the_result)           
-       
+def lyrics_output():
+
+    prefix = request.args.get('lyrics_topic')
+    sess = gpt2.start_tf_sess()
+    gpt2.load_gpt2(sess, run_name='run1')
+    
+    generated_text = gpt2.generate(sess, 
+                     length=250,
+                     temperature=0.9,
+                     prefix=prefix,
+                     nsamples=1,
+                     batch_size=1,
+                     return_as_list=True)[0]
+    
+    return generated_text
