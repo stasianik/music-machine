d8a985c47a71819d3b1d771105336f51d00b5f0b
Anastasia <anikouli@gmail.com>
2020-07-05 22:20:30 -0400

    Update files

---
+++ webapp/flaskexample/utils.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 22:33:46 2020
+
+@author: stasianik
+"""
+#import gpt_2_simple as gpt2
+from big_phoney import BigPhoney
+import lyricsgenius as genius
+import random
+import nlpaug.augmenter.word as naw
+import re
+
+#----------------For future live text generation-----------------
+# model_name = "124M"
+# model is saved into current directory under /models/124M/
+# gpt2.download_gpt2(model_name=model_name) #need to run only once. comment out once done.
+
+# Load models
+#sess = gpt2.start_tf_sess()
+#gpt2.load_gpt2(sess, run_name='run1')
+
+#Load text augmentation model
+#aug = naw.ContextualWordEmbsAug(
+#    model_path='bert-base-uncased', action="insert")
+#----------------------------------------------------------------
+
+def get_target_lyrics(artist, title):
+    
+    api_key = 'Nc3VjqRNlEZiD9mqOQATDQ5PuZ4IjdgV13n7c39OIyNwsjuazZThQlQkdr_Hts4c'
+    api = genius.Genius(api_key)
+    api.remove_section_headers = True # Removes section headers like "Verse" and "Intro"
+    
+    #get song lyrics
+    song = api.search_song(artist, title)
+    
+    #clean up the target lyrics
+    target = song.lyrics
+    target = target.replace("\u2005"," ")
+    target = target.replace("\\"," ")
+    target = target.replace("\\n"," ")
+    target = target.replace("("," ")
+    target = target.replace(")"," ")
+    target = target.replace("\n\n","\n")
+    target = target.replace("\n\n\n","\n")
+    target = target.replace("2x","")
+    #create list of lines
+    target_lyrics = target.split("\n")
+    #print("Target lyrics:")
+    #print(target_lyrics)
+    return target_lyrics
+
+#def generate_lyrics(): 
+#    
+#    gen_lyrics = gpt2.generate(sess, 
+#                     length=250,
+#                     temperature=0.9,
+#                     include_prefix=False,
+#                     #prefix="Deep in the night",
+#                     nsamples=1,
+#                     batch_size=1,
+#                     return_as_list=True)[0]
+#    
+#    gen_lyrics = gen_lyrics.split("\n")
+#    
+#    return gen_lyrics
+
+def lyrics_to_list(file_name):
+    with open(file_name, 'r') as file:
+        data = file.read().split("====================")
+        random_song = random.choice(data)
+        gen_lyrics = random_song.split("\n")
+        gen_lyrics = gen_lyrics[1:-1]
+#    print(gen_lyrics)
+    return gen_lyrics
+
+#This function uses the count_syls function above to keep track of num of syllables in each line
+def fit_lyrics(gen_lyrics, target_lyrics):
+    aug = naw.ContextualWordEmbsAug(
+    model_path='bert-base-uncased', action="insert")
+    phoney = BigPhoney()
+    
+    def count_syls(text):
+    
+        schema = []
+    
+        for line in text:
+            syls = phoney.count_syllables(line)
+            schema.append(syls)
+            #print(syls,line)
+        
+        return schema
+    def decontracted(phrase):
+        # specific
+        phrase = re.sub(r"won\'t", "will not", phrase)
+        phrase = re.sub(r"can\'t", "can not", phrase)
+
+        # general
+        phrase = re.sub(r"n\'t", " not", phrase)
+        phrase = re.sub(r"\'re", " are", phrase)
+        phrase = re.sub(r"\'s", " is", phrase)
+        phrase = re.sub(r"\'d", " would", phrase)
+        phrase = re.sub(r"\'ll", " will", phrase)
+        phrase = re.sub(r"\'t", " not", phrase)
+        phrase = re.sub(r"\'ve", " have", phrase)
+        phrase = re.sub(r"\'m", " am", phrase)
+        return phrase
+    
+    gen_schema = count_syls(gen_lyrics)
+    target_schema = count_syls(target_lyrics)
+#    print(target_schema)
+    #make generated lyrics same length as target lyrics
+    target_len = len(target_schema)
+    del gen_schema[target_len:]
+    del gen_lyrics[target_len:]
+    new_lyrics = []
+#    loop = 1
+    for num, line in enumerate(gen_lyrics):
+        
+        if (gen_schema[num] == target_schema[num]): 
+            new_lyrics.append(line)
+        elif (gen_schema[num] != target_schema[num]):
+            syls = gen_schema[num]
+            track = 0
+            while syls != target_schema[num]:
+                #while syls don't match the target
+                #make syls the next syls 
+                #if we're at the end then stop 
+                if track == len(gen_schema)-1:
+                    end = True
+                    break
+                else:
+                    track = track + 1
+                    syls = gen_schema[track]
+                    line = gen_lyrics[track]
+                    end = False
+            if end == False:
+                new_lyrics.append(line)
+            #If we can't find a line that's the right size
+            if end:
+                line = decontracted(line)
+#                print("No matching line")
+                #print(line)
+                while syls < target_schema[num]:
+#                    print("not enough syls")
+                    original_line = line
+                    line = aug.augment(line)
+#                    print(line)
+                    syls = phoney.count_syllables(line)
+                #In case we overshoot (add too many syllables)
+                    if syls > target_schema[num]:
+                        #print("Oops we overshot")
+                        line = original_line
+                        syls = phoney.count_syllables(line)
+                new_line = line
+#                print("the same line:")
+#                print(line)
+                syls = gen_schema[num]
+                words = line.split(" ")
+                while syls > target_schema[num]:
+#                    print("too many syls")
+                    del words[-1]
+                    new_line = ' '.join(words)
+                    syls = phoney.count_syllables(new_line)
+                    #In case too many syllables are deleted
+                    if syls < target_schema[num]:
+                        #print("Oops, deleted too many")
+                        new_line = aug.augment(new_line)
+                        syls = phoney.count_syllables(new_line)
+                new_lyrics.append(new_line)
+    return new_lyrics
--- webapp/flaskexample/views.py
+++ webapp/flaskexample/views.py
@@ -1,21 +1,8 @@
-from flask import render_template
-from flask import request
+from flask import render_template, request, Markup
 from flaskexample import app
-#from sqlalchemy import create_engine
-#from sqlalchemy_utils import database_exists, create_database
-#import pandas as pd
-#import psycopg2
-import gpt_2_simple as gpt2
-
-# Python code to connect to Postgres
-# You may need to modify this based on your OS, 
-# as detailed in the postgres dev setup materials.
-#user = 'stasianik' #add your Postgres username here      
-#host = 'localhost'
-#dbname = 'birth_db'
-#db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
-#con = None
-#con = psycopg2.connect(database = dbname, user = user)
+#import gpt_2_simple as gpt2
+from flaskexample.utils import lyrics_to_list, get_target_lyrics, fit_lyrics
+from collections import deque 
 
 # Load in model checkpoint and start a tensorflow session
 #sess = gpt2.start_tf_sess()
@@ -30,21 +17,46 @@ def index():
            
 @app.route('/input')
 def lyrics_input():
-   return render_template("input.html")
+   return render_template("input2.html")
 
 @app.route('/output')
 def lyrics_output():
+    artist = request.args.get('artist')
+    title = request.args.get('title')
+    target_lyrics = get_target_lyrics(artist, title)
+    
+    band = request.args.get('radioValue')
+    #connect to button 
+    if band == 'option1':
+        file_name = 'gpt2_gentext_slayer.txt'
+    elif band == 'option2':
+        file_name = 'gpt2_gentext_META.txt'
+    elif band == 'option3':
+        file_name = 'gpt2_gentext_IM.txt'
+    
+    gen_lyrics = lyrics_to_list(file_name)
+    #print(gen_lyrics)
+    new_lyrics = fit_lyrics(gen_lyrics, target_lyrics) 
+        
+    new_lyrics = deque(new_lyrics)
+    new_lyrics.appendleft('New Lyrics:')
+    lyrics1 = '<br>\n'.join(new_lyrics)
+    target_lyrics = deque(target_lyrics)
+    target_lyrics.appendleft('Target Lyrics:')
+    lyrics2 = '<br>\n'.join(target_lyrics)
+    combined = "<table><tr><td style='vertical-align:top; padding-right:20px'>" + lyrics1 + "</td><td style='vertical-align:top'>" + lyrics2 + '</td></tr></table>' 
 
-    prefix = request.args.get('lyrics_topic')
-    sess = gpt2.start_tf_sess()
-    gpt2.load_gpt2(sess, run_name='run1')
+# For future live model integration
+#    prefix = request.args.get('lyrics_topic')
+#    sess = gpt2.start_tf_sess()
+#    gpt2.load_gpt2(sess, run_name='run1')
+#    
+#    generated_text = gpt2.generate(sess, 
+#                     length=250,
+#                     temperature=0.9,
+#                     prefix=prefix,
+#                     nsamples=1,
+#                     batch_size=1,
+#                     return_as_list=True)[0]
     
-    generated_text = gpt2.generate(sess, 
-                     length=250,
-                     temperature=0.9,
-                     prefix=prefix,
-                     nsamples=1,
-                     batch_size=1,
-                     return_as_list=True)[0]
-    
-    return generated_text
+    return Markup(combined)
